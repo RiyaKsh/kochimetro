df131896a57e048ce4bdfc757aa74b4696fc39c8
aneeshack <aneeshack08@gmail.com>
2025-09-17 00:51:00 +0530

    added full app

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="w-full bg-white   shadow-[0px_4px_4px_0px_#015940] border-b-3 border-[#015940] ">
+    // <header className="w-full bg-white   shadow-[0px_4px_4px_0px_#015940] border-b-3 border-[#015940] ">
+    <header className="w-full bg-white border-b-3 border-blue ">
       <div className="max-w-7xl mx-auto flex items-center">
         <img 
           src="/logo.png"  
